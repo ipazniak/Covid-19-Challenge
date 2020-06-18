f71648507b1cf25765d7ab1efa6049d05324f967
ipazniak <ishmael.pazniak@icloud.com>
2020-06-18 11:30:06 +0300

    Update to Z99998.JCL(COVID19J).swift

--- Covid-19 Challenge/Z99998.JCL(COVID19J).swift
+++ Covid-19 Challenge/Z99998.JCL(COVID19J).swift
@@ -9,19 +9,19 @@ extension String {
     
     enum JCL {
         static let source = """
-//COVID19J JOB 1,NOTIFY=&SYSUID
+//\(String.Constants.dataSetMemberJCL) JOB 1,NOTIFY=&SYSUID
 //***************************************************/
 //COBRUN  EXEC IGYWCL
-//COBOL.SYSIN  DD DSN=&SYSUID..CBL(COVID19),DISP=SHR
-//LKED.SYSLMOD DD DSN=&SYSUID..LOAD(COVID19),DISP=SHR
+//COBOL.SYSIN  DD DSN=&SYSUID..CBL(\(String.Constants.dataSetMemberCBL)),DISP=SHR
+//LKED.SYSLMOD DD DSN=&SYSUID..LOAD(\(String.Constants.dataSetMemberCBL)),DISP=SHR
 //***************************************************/
 // IF RC = 0 THEN
 //***************************************************/
-//RUN     EXEC PGM=COVID19
+//RUN     EXEC PGM=\(String.Constants.dataSetMemberCBL)
 //STEPLIB   DD DSN=&SYSUID..LOAD,DISP=SHR
 //\(String.Constants.externalDataSet)  DD DSN=&SYSUID..\(String.Constants.dataSetExtensionPS),DISP=SHR
-//PRTLINE   DD SYSOUT=*,OUTLIM=15000
-//SYSOUT    DD SYSOUT=*,OUTLIM=15000
+//PRTLINE   DD SYSOUT=*,OUTLIM=1000
+//SYSOUT    DD SYSOUT=*,OUTLIM=5000
 //CEEDUMP   DD DUMMY
 //SYSUDUMP  DD DUMMY
 //***************************************************/
